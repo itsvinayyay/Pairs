d465504d95e3b4bfeb564e6696938f24a4fccf30
itsvinayyay <vinay.vashist2003@gmail.com>
2023-03-30 06:56:05 +0530

    Initial Commit

---
+++ lib/Screens/TourScreen/Tour_Screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Tour_Screen extends StatelessWidget {
+  const Tour_Screen({Key? key}) : super(key: key);
+
+  static const String id = "TourScreen";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
+import 'Screens/TourScreen/Tour_Screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Pairs());
+}
+
+class Pairs extends StatelessWidget {
+  const Pairs({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Tour_Screen(),
+      routes: {
+        Tour_Screen.id: (context) => Tour_Screen(),
+      },
+    );
+  }
 }
 
